1a3bae1a94d5c0df59588258f8302b267c322137
GillyDylan <contact@gilly-dylan.fr>
2019-06-17 10:22:15 +0200

    IdeaForm

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,7 +4,10 @@
 namespace App\Controller;
 
 
+use App\Entity\Idea;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends Controller
@@ -21,5 +24,19 @@ class MainController extends Controller
      */
     public function about(){
         return $this->render("about.html.twig");
+    }
+
+    /**
+     * @Route("/add", name="add")
+     */
+    public function add(Request $request){
+        $idea = new Idea();
+        $ideaForm = $this->createForm(Idea::class, $idea);
+        $ideaForm->handleRequest($request);
+        if($ideaForm->isSubmitted() && $ideaForm->isValid()){
+            $this->addFlash("succes", "Idea successfully added !");
+            $this->$this->redirectToRoute("test");
+        }
+        return $this->render("add.html.twig");
     }
 }
